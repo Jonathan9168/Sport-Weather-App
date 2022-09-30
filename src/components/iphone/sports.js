28ba0496a2fe208c5be2c0d99ea60dc5ef134e2b
Jonathan9168 <77795437+Jonathan9168@users.noreply.github.com>
2022-09-30 20:49:03 +0100

    Add files via upload

---
+++ src/components/iphone/sports.js
@@ -0,0 +1,8 @@
+export const Sports = {
+  golf: { name: "Golf" },
+  football: {name: "Football"},
+  cycling: {name: "Cycling"},
+  cricket: {name: "Cricket"},
+  baseball: {name: "Baseball"},
+  rowing: {name: "Rowing"}
+};
